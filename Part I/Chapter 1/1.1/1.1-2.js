486e7443bf85337c8a8e03b2a810480e2e9d3215
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2023-02-10 11:36:44 -0400

    1.1-2 and fixed the format of the 1.1-1

---
+++ Part I/Chapter 1/1.1/1.1-2.js
@@ -0,0 +1,8 @@
+// QUESTION
+    // Other than speed, what other measures of efficiency might one use in a real-world setting?
+
+// ANSWER
+    // - Use of RAM memory
+    // - Use of Storage
+    // - Error rate
+    // - Use of paralellism
