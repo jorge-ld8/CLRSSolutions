f6a8fb0b0b4b5d1a8c1f85fff5c501a78c8522d4
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2023-02-10 11:30:57 -0400

    exercise 1.1-1

---
+++ Part I/Chapter 1/1.1/1.1-1.js
@@ -0,0 +1,7 @@
+// Give a real-world example that requires sorting or a real-world example that requires computing a convex hull
+// SORTING
+// For example, companies that sell tickets for events and sports games such as Ticketmaster, SeatGeek or Ticket City,
+// in order to implement queues for highly demanded events they have to sort the users by the time they requested the tickets
+// so the firsts to be buying them will be the firsts that made the request.
+// CONVEX HULL
+// For instance, when you want to build a fence around a set of objects you'd want the convex hull to minimize the fencing used
