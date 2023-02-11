033ee6460a873856195bb7384e053859dbaadfa9
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2023-02-11 10:42:55 -0400

    1.1-3

---
+++ Part I/Chapter 1/1.1/1.1-3.js
@@ -0,0 +1,13 @@
+// QUESTION
+    // Select a data structure that you have seen previously, and discuss its strenghts and limitations.
+
+// ANSWER
+    // Linked lists 
+        // STRENGHTS
+            // - Unlike arrays uses the memory dynamically so it only occupies the storage it needs
+            // - They can be ordered using the same algorithms as used in arrays
+            // - Elements can be easily removed or reallocated.
+        // LIMITATIONS
+            // - They do not provide any form of efficient indexing, so operations like finding the last node in the list 
+            //   may have to iterate over the whole list.
+            // - They do not have an efficient way of accessing the date randomly
