f83b2f2264ac070550a9064dd9a6f1ba489637b1
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2023-02-11 11:40:44 -0400

    1.1-4

---
+++ Part I/Chapter 1/1.1/1.1-4.js
@@ -0,0 +1,11 @@
+// QUESTION
+    // How are the shortest-path and traveling-salesman problemas given above similar? How are they different?
+
+// ANSWER
+    // SIMILARITIES
+        // - The output of both is about minimizing the distance traveled
+    // DIFFERENCES
+        // - The Shortest Path problem input is just two nodes on a graph, while the traveling-salesman
+        //   is a set of points in a graph so the problem becomes harder because you have a lot more possibilites to travel between the points
+        // - The output of the shortest-path problem, is a set of nodes that resemble the shortest path to make going through all of points on the input, starting and ending on the same node,
+        //   while the shortest path is made up the set of nodes on a graph in which the first and the last one are the two nodes that make up the input
